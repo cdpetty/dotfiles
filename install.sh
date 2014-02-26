09fa4fd33cee68b83f3dcb72eee5fbdb8a0fb269
Clayton Petty <git.notyalc@gmail.com>
2014-02-25 22:32:57 -0600

    finished install.sh file. added error handling of some sort

--- install.sh
+++ install.sh
@@ -14,24 +14,36 @@ old_color="\033[33;40m"
 new_color="\033[32;40m"
 default_color="\033[37;40m"
 done="\033[31;40mDone\033[37;40m"
+
+#Error handling function
+set -e
+
 # create dorfiles_old in ~
-echo -e "Creating ${old_color}$olddir ${default_color}for backup of any existing dotfiles in ~... \\c"
-mkdir -p $olddir
+echo -e "Creating ${old_color}$olddir ${default_color}for backup of any existing dotfiles in ~... "
+mkdir $olddir
 echo -e "$done"
 
 # change directory into dotfiles
-echo -e "Changing to ${new_color}$dir ${default_color}directory... \\c"
+echo -e "Changing to ${new_color}$dir ${default_color}directory... "
 cd $dir
 echo -e "$done\n"
 
 # move old dotfiles into old_dotfiles directory AND create symlinks for new dotfiles directory
 for file in $files; do
-  echo -e "Moving old ${mv_color}$file ${default_color}from ~ to ${old_color}$olddir${default_color}... \\c"
-    mv ~/.$file $olddir/ 2> /dev/null
+  set +e
+  ls ~/.$file > /dev/null 2>&1
+  if [ "$?" == 0 ]; then
+    set -e
+    echo -e "Moving old ${mv_color}$file ${default_color}from ~ to ${old_color}$olddir${default_color}... "
+    mv ~/.$file $olddir/ # 2>&1 # 2> /dev/null
     echo -e "$done"
-    echo -e "Creating symlinks for new dotfile: ${ln_color}$file${default_color}... \\c"
-    ln -s $dir/$file ~/.$file 2> /dev/null
-    echo -e "$done \n"
+  else
+    set -e
+  fi
+  echo -e "Creating symlinks for new dotfile: ${ln_color}$file${default_color}... "
+  ln -s $dir/$file ~/.$file #2>&1
+  echo -e "$done \n"
 done
 
+trap "echo -e 'Finished. Symlinks to dotfiles configured. Old dotfiles located in directory: ${old_color}${olddir}'" EXIT
 # Done
